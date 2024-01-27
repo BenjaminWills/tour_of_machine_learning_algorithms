24589ed6e471d1b75706b0d62e6b3905030fd527
Benjaminwills <benjaminwills047@gmail.com>
2024-01-27 12:21:20 +0000

    Add exceptions and errors to cross entropy function

--- machine_learning_algorithms/loss_functions/cross_entropy.py
+++ machine_learning_algorithms/loss_functions/cross_entropy.py
@@ -27,6 +27,14 @@ def cross_entropy(predictions: List[np.array], truths: List[np.array]):
     # Calculate the number of datapoints
     num_data_points = len(predictions)
 
+    # If the lengths of the two arrays are not equal, raise a ValueError
+    if num_data_points != len(truths):
+        raise ValueError("The lengths of the two arrays should be equal")
+
+    # If some of the arrays are empty, raise value error
+    if num_data_points == 0:
+        raise ValueError("Empty arrays are not allowed")
+
     # Calculate the loss
     loss = 0
     for prediction, truth in zip(predictions, truths):
@@ -36,8 +44,17 @@ def cross_entropy(predictions: List[np.array], truths: List[np.array]):
         # truth     : [1, 0, 0, 0]
         # prediction: [0.8, 0.1, 0.05, 0.05]
 
+        # In the case of empty arrays, a value error is raised
+        if len(prediction) == 0 or len(truth) == 0:
+            raise ValueError("Empty arrays are not allowed")
+
         # Take the log of predictions
         log_predictions = np.log(prediction)
+
+        # if nan in log_predictions, replace it with an arbitrarily large value
+        # to avoid nan in the loss
+        log_predictions = np.nan_to_num(log_predictions, nan=1000)
+
         loss -= np.dot(truth, log_predictions)
 
     # Calculate average log loss.

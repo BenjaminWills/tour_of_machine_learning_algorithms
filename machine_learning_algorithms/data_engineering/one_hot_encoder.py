8aed38484a6d75534e0092f73625b66d953387e4
Benjaminwills <benjaminwills047@gmail.com>
2024-01-28 14:07:58 +0000

    Refactor one_hot_encode function to return both modified DataFrame and one-hot encoded column

--- machine_learning_algorithms/data_engineering/one_hot_encoder.py
+++ machine_learning_algorithms/data_engineering/one_hot_encoder.py
@@ -2,7 +2,16 @@ import pandas as pd
 
 
 def one_hot_encode(df, column_name):
+    """
+    One-hot encodes a categorical column in a DataFrame.
+
+    Parameters:
+    df (pandas.DataFrame): The DataFrame containing the categorical column.
+    column_name (str): The name of the categorical column to be one-hot encoded.
+
+    Returns:
+    tuple: A tuple containing the modified DataFrame with the categorical column dropped and the one-hot encoded column.
+    """
     one_hot_encoded = pd.get_dummies(df[column_name], dtype=int)
-    df = pd.concat([df, one_hot_encoded], axis=1)
     df.drop(column_name, axis=1, inplace=True)
-    return df
+    return df, one_hot_encoded

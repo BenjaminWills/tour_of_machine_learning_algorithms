2a455821a93309bf1521ff925a09a843569fa67e
Benjaminwills <benjaminwills047@gmail.com>
2024-01-28 14:08:07 +0000

    Add function to load categorical data

--- machine_learning_algorithms/data_engineering/data_loaders.py
+++ machine_learning_algorithms/data_engineering/data_loaders.py
@@ -1,8 +1,12 @@
 import pandas as pd
 import numpy as np
 
+from machine_learning_algorithms.data_engineering.one_hot_encoder import one_hot_encode
 
-def load_data(csv_path: str, dependent_column_name: str) -> np.ndarray:
+from typing import Tuple
+
+
+def load_data(csv_path: str, dependent_column_name: str) -> Tuple[np.ndarray]:
     """
     Load data from a CSV file and separate the dependent variable and independent variables.
 
@@ -20,3 +24,29 @@ def load_data(csv_path: str, dependent_column_name: str) -> np.ndarray:
     independent_variables = dataframe.drop([dependent_column_name], axis=1).to_numpy()
 
     return dependent_variable, independent_variables
+
+
+def load_categorical_data(
+    csv_path: str, classification_column_name: str
+) -> Tuple[np.ndarray]:
+    """
+    Load data from a CSV file and separate the classification column and independent variables.
+
+    Args:
+        csv_path (str): The path to the CSV file.
+        classification_column_name (str): The name of the classification column.
+
+    Returns:
+        np.ndarray: The classification column array.
+        np.ndarray: The independent variables array.
+    """
+    dataframe = pd.read_csv(csv_path, index_col=None)
+
+    dataframe, one_hot_encoded_categorical_column = one_hot_encode(
+        dataframe, classification_column_name
+    )
+
+    dependent_variables = dataframe.to_numpy()
+    categorical_data = one_hot_encoded_categorical_column.to_numpy()
+
+    return independent_variables, categorical_data

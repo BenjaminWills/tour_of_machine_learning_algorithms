45198f44f1ccf2fefe73adab71e1398fa173a7e7
Benjaminwills <benjaminwills047@gmail.com>
2024-01-31 21:27:54 +0000

    Update knn tests

--- tests/instance_based_algorithms/test_knn.py
+++ tests/instance_based_algorithms/test_knn.py
@@ -3,20 +3,22 @@ import numpy as np
 from machine_learning_algorithms.instance_based_algorithms.k_means.knn import (
     knn_classifier,
 )
+from sklearn.datasets import load_iris
+
+regression = load_iris()
+independent_variable = regression.data
+dependent_variable = regression.target
 
 
 class TestKNNClassifier(unittest.TestCase):
     def setUp(self):
         # Create a sample training dataset
-        self.training_data = np.array([[1, 2], [2, 3], [3, 4], [4, 5], [5, 6]])
-        self.classification_column = np.array([[0], [0], [1], [1], [1]])
         self.k = 3
-        self.knn = knn_classifier(self.training_data, self.classification_column)
+        self.knn = knn_classifier(independent_variable, dependent_variable)
 
     def test_find_k_nearest_neighbours(self):
         # Test case to check if the k nearest neighbors are correctly identified
-        input_data_point = np.array([3, 3])
-        expected_neighbours = [0, 1, 0]  # The first three data points belong to class 0
+        input_data_point = np.array([5.1, 3.5, 1.4, 0.2])
         most_common_neighbour_class = self.knn.find_k_nearest_neighbours(
             input_data_point, self.k
         )
@@ -24,13 +26,15 @@ class TestKNNClassifier(unittest.TestCase):
 
     def test_predict(self):
         # Test case to check if the classification labels are correctly predicted
-        input_data = np.array([[1, 2], [4, 5], [2, 3]])
+        input_data = np.array(
+            [[5.1, 3.5, 1.4, 0.2], [5.9, 3.0, 5.1, 1.8], [7.0, 3.2, 4.7, 1.4]]
+        )
         expected_predictions = [
             0,
+            2,
             1,
-            0,
         ]  # The first and third data points belong to class 0, the second data point belongs to class 1
-        predictions = self.knn.predict(input_data)
+        predictions = self.knn.predict(input_data, k=3)
         self.assertEqual(predictions, expected_predictions)
 
 

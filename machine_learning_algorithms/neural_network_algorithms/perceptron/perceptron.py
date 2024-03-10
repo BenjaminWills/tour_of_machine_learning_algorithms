e6ac801388ca7700e45427b7c4b73b6a90461f04
Benjaminwills <benjaminwills047@gmail.com>
2024-03-10 22:13:05 +0000

    Implement perceptron

--- machine_learning_algorithms/neural_network_algorithms/perceptron/perceptron.py
+++ machine_learning_algorithms/neural_network_algorithms/perceptron/perceptron.py
@@ -3,19 +3,11 @@ import numpy as np
 from typing import Callable
 
 
-def step_function(x: float) -> int:
-    return 1 if x >= 0 else 0
-
-
 class Perceptron:
-    def __init__(
-        self, weights: np.ndarray, bias: float, activation_function: Callable
-    ) -> None:
-        self.weights = weights
-        self.bias = bias
+    def __init__(self, weights: np.ndarray, activation_function: Callable) -> None:
+        self.weights = weights[:-1]
+        self.bias = weights[-1]
         self.activation_function = activation_function
 
     def predict(self, inputs: np.ndarray) -> int:
-        return step_function(
-            self.activation_function(np.dot(inputs, self.weights) + self.bias)
-        )
+        return self.activation_function(np.dot(inputs, self.weights) + self.bias)
